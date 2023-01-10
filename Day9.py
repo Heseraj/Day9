c5cbec5a4d777609cf610c49531a73927c7aaaba
HeSer <B@nd@r1359>
2023-01-10 15:46:54 -0500

    update v1

---
+++ Day9.py
@@ -0,0 +1,42 @@
+# This is the 9th day
+# Dictionaries
+# the game of bidders
+
+#%%
+programming_dictionary = {
+    "bug":"an error in a program that prevents the program from running as expected",
+    "function": "a piece of code that you cna esily call over and over again",
+}
+
+print(programming_dictionary["bug"])
+
+#%%
+# adding item to dictionaries
+
+programming_dictionary["loop"] = "the action of doing something over and over again."
+#%%
+print(programming_dictionary)
+
+#%% for looping in a dictionary
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
+
+
+
+#%%
+#%%
+#%%
+#%%
+#%%
+#%%
+#%%
+#%%
+#%%
+#%%
+#%%
+#%%
+#%%
+#%%
+#%%
+
